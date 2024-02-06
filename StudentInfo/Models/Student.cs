0cd0d6672d5a5c1dbc2c64ecf1f4acaab624ee68
Rishi Arora <rishiarora763@outlook.com>
2024-02-06 16:21:23 +0530

    Add project files.

---
+++ StudentInfo/Models/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo.Models
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _name;
+        
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+}
---
+++ StudentInfo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using StudentInfo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private Student _model;
+        private string _name;
+
+        public MainWindowViewModel()
+        {
+            _model = new Student()
+            {
+                Name = "Rishi Arora"
+            };
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChange("Name");
+            }
+        }
+
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); 
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
